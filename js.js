ab05609a124e8bd91c699bf1b9191b400dbb2d01
Mohammed-Riad <hamodehriad77@gmail.com>
2022-08-11 19:48:18 +0300

    add

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 
-debugger
+// debugger
 let name;
 name=prompt("what your name")
 let gender = prompt("what your gender");
@@ -34,6 +34,103 @@ function age()
     let age=prompt("what your age");
     if(age <= 0)
     {
-     alert("hamodeh")
+     
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+    let arr=[];
+    let answer =prompt("are you an otaku")
+    if(answer=="yes" || answer=="y" || answer=="no" ||answer=="n")
+    {
+       arr.push(answer)
+
+
+       let answer1=prompt("Is your favorite anime One Piece?")
+       if(answer1=="yes" || answer1=="y" || answer1=="no" ||answer1=="n")
+       {
+        arr.push(answer1)
+
+
+
+        let answer2=prompt("I watch anime for 3 hours, are you like me?")
+        if(answer2=="yes" || answer2=="y" || answer2=="no" ||answer2=="n")
+        {
+            arr.push(answer2)
+        }else
+        {
+            arr.push("invaild")
+        }
+
+
+
+       }else
+       {
+        arr.push("invaild")
+        let answer2=prompt("I watch anime for 3 hours, are you like me?")
+        if(answer2=="yes" || answer2=="y" || answer2=="no" ||answer2=="n")
+        {
+            arr.push(answer2)
+        }else
+        {
+            arr.push("invaild")
+        }
+
+       }
+
+        
+
+
+       
+    }
+    else{
+        arr.push("invaild")
+        let answer1=prompt("Is your favorite anime One Piece?")
+        if(answer1=="yes" || answer1=="y" || answer1=="no" ||answer1=="n")
+        {
+         arr.push(answer1)
+         let answer2=prompt("I watch anime for 3 hours, are you like me?")
+         if(answer2=="yes" || answer2=="y" || answer2=="no" ||answer2=="n")
+         {
+            arr.push(answer2)
+         }else
+         {
+            arr.push("invaild")
+         }
+ 
+        }else
+        {
+         arr.push("invaild")
+         let answer2=prompt("I watch anime for 3 hours, are you like me?")
+         if(answer2=="yes" || answer2=="y" || answer2=="no" ||answer2=="n")
+         {
+            arr.push(answer2)
+         }else
+         {
+            arr.push("invaild")
+         }
+ 
+        }
+        
+        
+    }
+    console.log(arr)
+
+
+let star =prompt("how many star do you rate")
+if(star < 5)
+{
+    alert("why"+""+star)
+}else if(star >= 5)
+{
+alert("thxx for rate "+""+star)
 }
